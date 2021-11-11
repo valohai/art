ec27505480602f2cbef0299ec2d9a2d7601786f6
Aarni Koskela <akx@iki.fi>
2021-11-11 14:55:47 +0200

    Quiesce pytest mocker warning

--- art_tests/test_s3.py
+++ art_tests/test_s3.py
@@ -7,9 +7,9 @@ from art.write import _write_file
 def test_s3_acl(mocker):
     cli = get_s3_client()
     cli.put_object = cli.put_object  # avoid magic
-    with mocker.patch.object(cli, "put_object"):
-        body = io.BytesIO(b"test")
-        _write_file("s3://bukkit/key", body, options={"acl": "public-read"})
-        cli.put_object.assert_called_with(
-            Bucket="bukkit", Key="key", ACL="public-read", Body=body
-        )
+    mocker.patch.object(cli, "put_object")
+    body = io.BytesIO(b"test")
+    _write_file("s3://bukkit/key", body, options={"acl": "public-read"})
+    cli.put_object.assert_called_with(
+        Bucket="bukkit", Key="key", ACL="public-read", Body=body
+    )
