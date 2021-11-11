98a2e0b67378e8435bd03915149251d289bd9b93
Aarni Koskela <akx@iki.fi>
2021-11-11 13:54:19 +0200

    Add missing wheel test dependency

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 from art import __version__
 
-tests_require = ['pytest', 'pytest-cov', 'pytest-mock']
+tests_require = ['pytest', 'pytest-cov', 'pytest-mock', 'wheel']
 setup(
     name='art',
     description='Artifact manager',
