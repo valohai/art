2b0d6877c20370ef3ec2acdd5bbdf3642ef4394a
Aarni Koskela <akx@iki.fi>
2021-11-11 14:02:49 +0200

    Move development dependencies to requirements-dev

--- setup.py
+++ setup.py
@@ -4,17 +4,14 @@ from setuptools import setup
 
 from art import __version__
 
-tests_require = ['pytest', 'pytest-cov', 'pytest-mock', 'wheel']
 setup(
-    name='art',
-    description='Artifact manager',
-    author='Valohai',
-    author_email='hait@valohai.com',
-    license='MIT',
+    name="art",
+    description="Artifact manager",
+    author="Valohai",
+    author_email="hait@valohai.com",
+    license="MIT",
     version=__version__,
-    packages=['art'],
-    entry_points={'console_scripts': ['art=art.command:run_command']},
-    install_requires=['boto3', 'pyyaml'],
-    tests_require=tests_require,
-    extras_require={'test': tests_require},
+    packages=["art"],
+    entry_points={"console_scripts": ["art=art.command:run_command"]},
+    install_requires=["boto3", "pyyaml"],
 )
