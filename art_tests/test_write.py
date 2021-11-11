138bd391a9e2457b84e110b44158560eed01798a
Aarni Koskela <akx@iki.fi>
2021-11-11 14:06:55 +0200

    Blacken, isort

--- art_tests/test_write.py
+++ art_tests/test_write.py
@@ -1,13 +1,15 @@
+import art.write
 from art.config import ArtConfig
 from art.manifest import Manifest
-import art.write
 
 
 def test_dest_options(mocker):
     cfg = ArtConfig()
     mf = Manifest(files={})
-    wf = mocker.patch('art.write._write_file')
-    art.write.write(cfg, dest='derp://foo/bar/?acl=quux', path_suffix='blag', manifest=mf)
+    wf = mocker.patch("art.write._write_file")
+    art.write.write(
+        cfg, dest="derp://foo/bar/?acl=quux", path_suffix="blag", manifest=mf
+    )
     call_kwargs = wf.call_args[1]
-    assert call_kwargs['options'] == {'acl': 'quux'}
-    assert call_kwargs['dest'] == 'derp://foo/bar/blag/.manifest.json'
+    assert call_kwargs["options"] == {"acl": "quux"}
+    assert call_kwargs["dest"] == "derp://foo/bar/blag/.manifest.json"
